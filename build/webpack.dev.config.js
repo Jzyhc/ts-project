8d726d63d62fc586381b9c46b766f31432d7b5ef
jincheng <jincheng@soyoung.com>
2020-03-11 16:22:00 +0800

    first commit

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,14 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// module.exports = {
+//     entry: './src/index.ts',
+//     devtool: 'cheap-module-eval-source-map',
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             template: './src/tpl/index.html'
+//         })
+//     ]
+// }
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+}
